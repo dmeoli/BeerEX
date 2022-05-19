7c8fcd32787203498bb6bf9b0b8fa01d1bad2beb
Donato Meoli <donato.meoli.95@gmail.com>
2022-05-19 22:37:47 +0200

    replaced pyclips with clipspy lib

--- BeerEX.bot.py
+++ BeerEX.bot.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 env = clips.Environment()
 
 
-def start(bot, update):
+def start(update, context):
     """
     Sends a welcome message when the command /start is issued.
     """
@@ -37,17 +37,17 @@ def start(bot, update):
     env.run()
     update.message.reply_text(text='Hello %s! 🤖' % update.message.from_user.first_name,
                               reply_markup=ReplyKeyboardRemove())
-    update.message.reply_text(env.eval('(find-fact ((?u UI-state)) (eq ?u:state initial))')[0].Slots['display'])
+    update.message.reply_text(env.eval('(find-fact ((?u UI-state)) (eq ?u:state initial))')[0]['display'])
 
 
-def new(bot, update):
+def new(update, context):
     """
     Starts a new chat with the beer expert when the command /new is issued.
     """
 
     env.reset()
     env.run()
-    nextUIState(bot, update)
+    nextUIState(context.bot, update)
 
 
 def nextUIState(bot, update):
@@ -55,53 +55,53 @@ def nextUIState(bot, update):
     Re-creates the dialog window to match the current state in working memory.
     """
 
-    current_id = env.eval('(find-fact ((?s state-list)) TRUE)')[0].Slots['current']
+    current_id = env.eval('(find-fact ((?s state-list)) TRUE)')[0]['current']
     current_ui = env.eval('(find-fact ((?u UI-state)) (eq ?u:id %s))' % current_id)
-    state = current_ui[0].Slots['state']
+    state = current_ui[0]['state']
     if state == 'initial':
-        env.Assert('(next %s)' % current_id)
+        env.assert_string('(next %s)' % current_id)
         env.run()
         nextUIState(bot, update)
     elif state == 'final':
-        keyboard = [[KeyboardButton(text=emojize(':back: Previous', use_aliases=True))],
-                    [KeyboardButton(text=emojize(':repeat: Restart', use_aliases=True))],
-                    [KeyboardButton(text=emojize(':x: Cancel', use_aliases=True))]]
-        update.message.reply_text(text=current_ui[0].Slots['display'],
+        keyboard = [[KeyboardButton(text=emojize(':back: Previous', language='alias'))],
+                    [KeyboardButton(text=emojize(':repeat: Restart', language='alias'))],
+                    [KeyboardButton(text=emojize(':x: Cancel', language='alias'))]]
+        update.message.reply_text(text=current_ui[0]['display'],
                                   parse_mode=ParseMode.MARKDOWN,
                                   disable_web_page_preview=True,
                                   reply_markup=ReplyKeyboardMarkup(keyboard))
     else:
         keyboard = list()
-        for answer in current_ui[0].Slots['valid-answers']:
+        for answer in current_ui[0]['valid-answers']:
             keyboard.append([KeyboardButton(text=answer)])
-        if current_ui[0].Slots['help']:
-            keyboard.append([KeyboardButton(text=emojize(':sos: Help', use_aliases=True))])
-        if current_ui[0].Slots['why']:
-            keyboard.append([KeyboardButton(text=emojize(':question: Why', use_aliases=True))])
-        if len(env.eval('(find-fact ((?s state-list)) TRUE)')[0].Slots['sequence']) > 2:
-            keyboard.append([KeyboardButton(text=emojize(':back: Previous', use_aliases=True))])
-        keyboard.append([KeyboardButton(text=emojize(':x: Cancel', use_aliases=True))])
-        update.message.reply_text(text=current_ui[0].Slots['display'],
+        if current_ui[0]['help']:
+            keyboard.append([KeyboardButton(text=emojize(':sos: Help', language='alias'))])
+        if current_ui[0]['why']:
+            keyboard.append([KeyboardButton(text=emojize(':question: Why', language='alias'))])
+        if len(env.eval('(find-fact ((?s state-list)) TRUE)')[0]['sequence']) > 2:
+            keyboard.append([KeyboardButton(text=emojize(':back: Previous', language='alias'))])
+        keyboard.append([KeyboardButton(text=emojize(':x: Cancel', language='alias'))])
+        update.message.reply_text(text=current_ui[0]['display'],
                                   reply_markup=ReplyKeyboardMarkup(keyboard))
 
 
-def handleEvent(bot, update):
+def handleEvent(update, context):
     """
     Triggers the next state in working memory based on which button is pressed.
     """
 
-    current_id = env.eval('(find-fact ((?s state-list)) TRUE)')[0].Slots['current']
+    current_id = env.eval('(find-fact ((?s state-list)) TRUE)')[0]['current']
     current_ui = env.eval('(find-fact ((?u UI-state)) (eq ?u:id %s))' % current_id)
     response = update.message.text
-    if response in current_ui[0].Slots['valid-answers']:
+    if response in current_ui[0]['valid-answers']:
         if len(response.split(' ')) > 1:
-            env.Assert('(next %s "%s")' % (current_id, response))
+            env.assert_string('(next %s "%s")' % (current_id, response))
         else:
-            env.Assert('(next %s %s)' % (current_id, response))
-        env.Run()
-        nextUIState(bot, update)
-    elif response == emojize(':sos: Help', use_aliases=True):
-        help = current_ui[0].Slots['help']
+            env.assert_string('(next %s %s)' % (current_id, response))
+        env.run()
+        nextUIState(context.bot, update)
+    elif response == emojize(':sos: Help', language='alias'):
+        help = current_ui[0]['help']
         if not re.findall('_.+?_\(.*?\)', help):
             update.message.reply_text(text=help,
                                       parse_mode=ParseMode.MARKDOWN)
@@ -115,46 +115,46 @@ def handleEvent(bot, update):
             update.message.reply_text(text=help,
                                       parse_mode=ParseMode.MARKDOWN,
                                       reply_markup=InlineKeyboardMarkup(keyboard))
-    elif response == emojize(':question: Why', use_aliases=True):
-        update.message.reply_text(text=current_ui[0].Slots['why'],
+    elif response == emojize(':question: Why', language='alias'):
+        update.message.reply_text(text=current_ui[0]['why'],
                                   parse_mode=ParseMode.MARKDOWN)
-    elif response == emojize(':back: Previous', use_aliases=True):
-        env.Assert('(prev %s)' % current_id)
-        env.Run()
-        nextUIState(bot, update)
-    elif response == emojize(':repeat: Restart', use_aliases=True):
-        new(bot, update)
-    elif response == emojize(':x: Cancel', use_aliases=True):
-        env.Reset()
+    elif response == emojize(':back: Previous', language='alias'):
+        env.assert_string('(prev %s)' % current_id)
+        env.run()
+        nextUIState(context.bot, update)
+    elif response == emojize(':repeat: Restart', language='alias'):
+        new(update, context)
+    elif response == emojize(':x: Cancel', language='alias'):
+        env.reset()
         update.message.reply_text(text='Bye! I hope we can talk again some day. 👋🏻',
                                   reply_markup=ReplyKeyboardRemove())
 
 
-def imageButton(bot, update):
+def imageButton(update, context):
     """
     Sends help images based on which button is pressed.
     """
 
     query = update.callback_query
-    bot.send_photo(chat_id=query.message.chat_id,
-                   photo=query.data)
+    context.bot.send_photo(chat_id=query.message.chat_id,
+                           photo=query.data)
 
 
-def unknown(bot, update):
+def unknown(update, context):
     """
     Sends an error message when an unrecognized command is typed.
     """
 
-    bot.send_message(chat_id=update.message.chat_id,
-                     text='Unrecognized command. Say what?')
+    context.bot.send_message(chat_id=update.message.chat_id,
+                             text='Unrecognized command. Say what?')
 
 
-def error(bot, update, error):
+def error(update, context):
     """
     Log errors caused by updates.
     """
 
-    logger.warning('Update %s caused error %s' % (update, error))
+    logger.warning('Update %s caused error %s', update, context.error)
 
 
 def main():
@@ -189,7 +189,7 @@ def main():
                           port=int(os.environ.get('PORT', '5000')),
                           url_path=token,
                           webhook_url='https://beerex-telegram-bot.herokuapp.com/' + token)
-    # updater.start_polling()
+    # updater.start_polling()  # to run in local enable this and comment the previous line
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT. This should
     # be used most of the time, since start_polling() is non-blocking and will stop the bot gracefully.
